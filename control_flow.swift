5fb7e9f802045a40d02d20e3ab6bc6d840528dd0
Hasan <hasanozdemir1@trakya.edu.tr>
2022-04-02 14:54:42 +0300

    control-flows

---
+++ control_flow.swift
@@ -0,0 +1,16 @@
+/* 
+this file is created at 04-02-2022
+by Hasan Özdemir
+*/
+
+import Swift
+
+let temperatureInFahrenheit: Int = 90
+
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else if temperatureInFahrenheit >= 86 {
+    print("It's really warm. Don't forget to wear sunscreen.")
+} else {
+    print("It's not that cold. Wear a t-shirt.")
+}
